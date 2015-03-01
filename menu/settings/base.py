17a16781bf3afebedb07e464a6ac34326265b3f1
Paul Sherman <paul.sherman.88@gmail.com>
2015-02-28 20:16:29 -0600

    configuring things for heroku

--- menu/settings/base.py
+++ menu/settings/base.py
@@ -47,6 +47,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 LOGIN_URL = "/accounts/login"
 LOGIN_REDIRECT_URL = "/"
