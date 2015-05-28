608872970bcc1827aaf73b8af8d5e7f7f3164b2d
Paul Sherman <paul.sherman.88@gmail.com>
2015-05-28 00:07:07 -0500

    init commit

---
+++ workouts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+"""
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Workout(models.Model):
+    exercise = models.ForeignKey(Exercise)
+    date = models.DateTimeField('workout date')
+"""
