1753bb9c2671c67e42d0c1c07eafb1e76ebbce53
Paul Sherman <paul.sherman.88@gmail.com>
2015-08-18 23:25:25 -0500

    nunjucks!

--- routes.js
+++ routes.js
@@ -1,12 +1,3 @@
-module.exports = {
-    index: function(req, res) {
-        res.sendFile(__dirname + "/views/index.html");
-    },
-    room: function(req, res) {
-        var code = req.params.code;
-        console.log(code);
-        res.render("room", {
-            code: code
-        });
-    }
+module.exports.index =  function(req, res) {
+    res.render("index.html");
 };
--- server.js
+++ server.js
@@ -3,17 +3,18 @@ var http = require('http');
 var socket = require('socket.io');
 var routes = require('./routes');
 var gamemaster = require('./utils/gamemaster');
-var handlebars = require('express-handlebars');
+var nunjucks = require('nunjucks');
 
 var app = express();
 var server = http.Server(app);
 var io = socket(server);
 
-app.engine('handlebars', handlebars({defaultLayout: 'base'}));
-app.set('view engine', 'handlebars');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 app.get('/', routes.index);
-app.get('/room/:code', routes.room);
 app.use('/static', express.static(__dirname + "/public/"));
 gamemaster(io);
 
