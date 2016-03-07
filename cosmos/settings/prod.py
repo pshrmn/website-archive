28aa239277ca4aa4464a6d75bd887c5814d7526e
Paul Sherman <paul.sherman.88@gmail.com>
2016-03-07 16:22:59 -0600

    setup for heroku

--- cosmos/settings/prod.py
+++ cosmos/settings/prod.py
@@ -3,12 +3,11 @@ import dj_database_url
 
 from .base import *
 
-# False if environment variable isn't set
-DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
+DEBUG = False
 
 ADMINS = (('Admin', os.environ['ADMIN_EMAIL']),)
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['*']
